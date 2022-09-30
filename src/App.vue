80db47e3eb7e23db6368ba16a69c74bab8e02928
Victor <a3518110554@alumno.uttehuacan.edu.mx>
2022-09-30 13:18:12 -0500

    formulario

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container mt-3">
+   <Header />
+  <Formulario />
+    <!-- <form @submit.prevent="addUser">
+      <div>
+        <label>
+        nombre: <input v-model="nombre" type="text" placeholder="Nombre" required/>
+      </label>
+      </div>
+      <div>
+        <label>
+        edad: <input v-model="edad" type="number" min="15" max="80" placeholder="edad" required/>
+      </label>
+      </div>
+      <div>
+        <label>
+        correo: <input v-model="corro" type="email" placeholder="gmail@gmail.com " required/>
+      </label>
+      </div>
+      
+      <button type="submit">Enviar</button>
+    </form> -->
+  </div>
+</template>
+
+<script>
+  import Header from "./components/Header.vue";
+  import Formulario from "./components/Formulario.vue";
+
+  export default{
+    components: {
+      Header,
+      Formulario
+    }
+  }
+</script>
+
+
+
